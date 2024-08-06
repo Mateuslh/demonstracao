61e2a691031cc131aeea34bfd2c96d58e54b7717
Mateuslh <mateuslealhemkemeier@gmail.com>
2024-08-06 02:06:23 -0300

    [feat] - Criado os models. - Divida - Debito - Contribuinte

---
+++ src/main/java/com/projetoDemonstracao/demonstracao/domain/Contribuinte.java
@@ -0,0 +1,29 @@
+package com.projetoDemonstracao.demonstracao.domain;
+
+import com.projetoDemonstracao.demonstracao.enums.SituacaoCadastro;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contribuinte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    private Long codigo;
+
+    @Enumerated(EnumType.STRING)
+    private SituacaoCadastro SituacaoCadastro;
+
+    private String nome;
+
+    private String email;
+}
---
+++ src/main/java/com/projetoDemonstracao/demonstracao/domain/Debito.java
@@ -0,0 +1,35 @@
+package com.projetoDemonstracao.demonstracao.domain;
+
+import com.projetoDemonstracao.demonstracao.enums.SituacaoGuia;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Debito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Contribuinte contribuinte;
+
+    @Enumerated(EnumType.STRING)
+    private SituacaoGuia situacaoGuia;
+
+    private BigDecimal valorLancado;
+
+    private BigDecimal valorDesconto;
+
+    private BigDecimal valorAcrescimo;
+}
